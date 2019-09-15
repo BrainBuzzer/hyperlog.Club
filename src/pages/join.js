ebefa006e9c6cbf47b1ccd583dafbc7fd279d547
Aditya <dtrg21@gmail.com>
2019-09-15 11:53:03 +0530

    Add join page

---
+++ src/pages/join.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Join hyperlog.Club" />
+    <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script><iframe class="airtable-embed airtable-dynamic-height" src="https://airtable.com/embed/shriOya7CEfYhBRqU?backgroundColor=orange" frameborder="0" onmousewheel="" width="100%" height="704" style={{background: `transparent`}}></iframe>
+  </Layout>
+)
+
+export default IndexPage
