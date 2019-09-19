df14122287129fb8011564288c6ed79d147859b9
Aditya <dtrg21@gmail.com>
2019-09-19 07:20:29 +0530

    Fix the Hero component

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,11 +1,14 @@
 import React from "react"
 import styled from "@emotion/styled"
 import BackgroundImage from "gatsby-background-image"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import { Icon } from "antd"
 
 const ImageBackground = styled(BackgroundImage)`
-  background-size: cover;
-  height: 400px;
+  background-repeat: repeat;
+  background-size: inherit;
+  height: 500px;
+  z-index: 999;
 `
 
 const TextBox = styled('div')`
@@ -17,28 +20,103 @@ const TextBox = styled('div')`
   padding: 0 calc((100vw-550px)/2) 2rem;
   width: 100%;
 
-  h1 {
-    text-shadow: 1px 1px 3px #3366ff66;
+  .header-logo {
+    padding: 2px;
+    background: rgba(255,255,255,.9);
     font-size: 2.25rem;
+  }
+
+  .hero-text {
+    max-width: 600px;
+    margin-top: 20px;
+    text-align: center;
+
+    p {
+      color: #fff;
+      display: inline;
+      padding: 0.1em;
+      background: rgba(0,0,0,.5);
+      font-size: 1.2rem;
+    }
+  }
+
+  .hero-buttons {
+    display: flex;
+    flex-direction: row;
+  }
+`
+
+const Button = styled(Link)`
+  border-radius: 5px;
+  color: #fff;
+  display: block;
+  font-size: 1.6em;
+  margin: 1em auto;
+  margin-left: 10px;
+  padding: 0.4em;
+  position: relative;
+  transition: ease;
+  background: rgba(255,255,255,0.5);
+
+  &.join-button {
+    border: 3px solid #fff;
+    font-weight: 600;
+  }
+
+  &.resource-button {
+    border: 1px solid #fff;
+    font-weight: 400;
+  }
+
+  &::after {
+    height: 0;
+    left: 0;
+    top: 0;
+    width: 100%;
+  }
+
+  &::before, &::after {
+    background: #fff;
+    content: '';
+    position: absolute;
+    z-index: -1;
+  }
+
+  &:hover {
+    color: #2ecc71;
+    box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.5);
+    transition: ease;
+
+    &:after {
+      height: 100%;
+    }
   }
 `
 
 const Hero = () => {
   const { bgimage } = useStaticQuery(graphql`
   query {
-    bgimage: file(relativePath:{eq: "home-hero-bg.jpg"}){
+    bgimage: file(relativePath:{eq: "hero-bg.png"}){
       childImageSharp{
-        fluid(grayscale: true) {
+        fluid {
           ...GatsbyImageSharpFluid
         }
       }
     }
   }
   `)
+
+  // TODO!important: Fix the styling of buttons
+
   return (
     <ImageBackground Tag="section" fluid={bgimage.childImageSharp.fluid}>
       <TextBox>
-        <h1>hyperlog.Club</h1>
+        <div className="header-logo">h<span className="header-logo-letter-y">y</span>perlog<span className="header-logo-club">.Club</span></div>
+        <div className="hero-text"><p>We're an open community of newbie programmers who learn to code together.</p></div>
+        <div className="hero-buttons">
+          <Button className="join-button">Join Community <Icon type="arrow-right" /></Button>
+          <Button className="resource-button">Learn More</Button>
+        </div>
       </TextBox>
     </ImageBackground>
   );
