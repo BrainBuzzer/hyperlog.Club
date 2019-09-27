a7b62715dbdb120e31915344d9cc2babec54ea81
Aditya <dtrg21@gmail.com>
2019-09-27 13:32:29 +0530

    Add loading spinner

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,10 +1,13 @@
 import React from "react"
 import { handleAuthentication } from "../utils/auth"
+import { Spin, Icon } from 'antd';
+
+const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
 const Callback = () => {
   handleAuthentication()
 
-  return <p>Loading...</p>
+  return <Spin indicator={antIcon} />
 }
 
 export default Callback
