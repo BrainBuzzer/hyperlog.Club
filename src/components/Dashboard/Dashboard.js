3d5a494d05f79597d5c522bd9edd78ad4acf845a
Aditya Giri <dtrg21@gmail.com>
2019-10-12 11:14:19 +0530

    Add proper dashboard for users

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  overflow: none;
+  text-align: center;
+`;
 
 class Dashboard extends React.Component {
   render() {
     return(
       <>
-        <span>Dashboard</span>
+        <Container>
+          <h1>🤫We're working on something amazing</h1>
+        </Container>
       </>
     )
   }
